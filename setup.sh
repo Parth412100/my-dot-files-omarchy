8b9e7136ff10c29ed6c2ef0c8ae5cc1fa2c2be4f
parth <parth412100@gmail.com>
2025-12-07 11:22:53 +0530

    updated setup.sh

--- setup.sh
+++ setup.sh
@@ -13,7 +13,49 @@ rm ~/.local/share/applications/HEY.desktop  #removing hey
 sudo pacman -Rns --noconfirm signal-desktop obsidian 1password-beta 1password-cli
 
 echo "Installing required packages..."
-yay -S --noconfirm --needed mpv fastfetch btop git yt-dlp ani-cli blueman zen-browser-bin lvsk-calendar visual-studio-code-bin
+yay -S --noconfirm --needed mpv fastfetch rofi btop git yt-dlp ani-cli blueman zen-browser-bin lvsk-calendar visual-studio-code-bin
 
-. ./calender.sh
+#-------------------------
+# 2. Removing dotfiles
+# ------------------------
+
+echo "removing config dotfiles"
+cd $HOME/.config
+rm -rf btop fastfetch git hypr kitty lvsk-calendar omarchy waybar zen
+
+#-------------------------
+# 3. adding dotfiles
+# ------------------------
+
+echo "adding config dotfiles"
+cd $HOME/.config
+
+#btop
+cp -rf $HOME/dotfiles/btop $HOME/.config/
+
+#fastfetch
+cp -rf $HOME/dotfiles/fastfetch $HOME/.config/
+
+#git
+cp -rf $HOME/dotfiles/git $HOME/.config/
+
+#hypr
+cp -rf $HOME/dotfiles/hypr $HOME/.config/
+
+#kitty
+cp -rf $HOME/dotfiles/kitty $HOME/.config/
+
+#lvsk-calendar
+cp -rf $HOME/dotfiles/lvsk-calendar $HOME/.config/
+
+#omarchy
+cp -rf $HOME/dotfiles/omarchy $HOME/.config/
+
+#waybar
+cp -rf $HOME/dotfiles/waybar $HOME/.config/
+
+#zen
+cp -rf $HOME/dotfiles/zen $HOME/.config/
+
+
 
